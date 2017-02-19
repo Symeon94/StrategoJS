6845461545d7b424f1d680ab49a6ee4257524083
Symeon94 <symeon94@hotmail.com>
2017-02-19 15:07:49 +0100

    Initiatial push

---
+++ src/system/System.java
@@ -0,0 +1,53 @@
+package system;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.*;
+import org.lwjgl.util.glu.GLU;
+
+import static org.lwjgl.opengl.GL11.*;
+
+public class System 
+{
+	public static void main(String[] args) throws LWJGLException
+	{
+		Display.setDisplayMode(new DisplayMode(800,600));
+		Display.setFullscreen(false);
+		Display.setVSyncEnabled(true);
+		Display.create();
+		
+		init();
+		
+		while(!Display.isCloseRequested())
+		{
+			update();
+			render();
+			Display.update();
+		}
+		Display.destroy();
+	}
+	
+	public static void init() {
+		glEnable(GL_TEXTURE_2D);
+		glEnable(GL_CULL_FACE);
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		GLU.gluOrtho2D(0, 800, 0, 600);
+	}
+	
+	public static void update() {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0, 0, 0, 1);
+		glMatrixMode(GL_MODELVIEW);
+	}
+	
+	public static void render() {
+		glTranslatef(0.1f,0,0);
+		glBegin(GL_TRIANGLES);
+		glColor3f(1,1,1);
+		glVertex3f(0,0,0);
+		glVertex3f(400,0,0);
+		glVertex3f(0,300,0);
+		glEnd();
+	}
+	
+}
